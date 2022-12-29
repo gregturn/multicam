be70b6ff5ffe192537ba3f262f96556635a5f523
Greg L. Turnquist <gturnquist@vmware.com>
2022-12-28 21:13:44 -0600

    Fix library.location to be an attribute.

--- src/main/java/com/springbootlearning/multicam/Library.java
+++ src/main/java/com/springbootlearning/multicam/Library.java
@@ -1,6 +1,8 @@
 package com.springbootlearning.multicam;
 
-public record Library(String location, Event event) {
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+
+public record Library(@JacksonXmlProperty(isAttribute = true) String location, Event event) {
 
 	public static Library transform(com.springbootlearning.recut.Library library) {
 		return new Library(library.location(), Event.transform(library.event()));
